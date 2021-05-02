bf7f5f6ea03a5f7e3020617805ac3043d566847b
Thomas <kaulketh@gmail.com>
2021-05-02 22:03:51 +0200

    Update: improved value representation and appearance

--- oled.py
+++ oled.py
@@ -19,17 +19,17 @@ from luma.oled.device import ssd1306
 from dht import get_values
 
 serial = i2c(port=1, address=0x3C)
-oled = ssd1306(serial)
+display = ssd1306(serial)
 SPACE = " "
 left = 0
 top = 0
 
 # Load fonts
-# font = ImageFont.load_default()  # Load default font
+# font = ImageFont.load_default()
 # Alternatively load a TTF font.
 # Some other nice fonts to try: http://www.dafont.com/bitmap.php
 
-font_dir = "/home/pi/oled/fonts/"
+__font_folder = "/home/pi/oled/fonts/"
 
 
 def __load_font(font_file, size):
@@ -37,35 +37,63 @@ def __load_font(font_file, size):
      To avoid issue related to how Unicode file paths are handled on platforms,
      read the font file in binary form.
     """
-
     with open(font_file, "rb") as f:
         bytes_font = BytesIO(f.read())
     return ImageFont.truetype(bytes_font, size)
 
 
-tahoma = __load_font(f"{font_dir}tahoma.ttf", 30)
-dejavu = __load_font(f"{font_dir}dejavu.ttf", 28)
-arial_10 = __load_font(f"{font_dir}arial.ttf", 10)
-arial_12 = __load_font(f"{font_dir}arial.ttf", 12)
-free_sans_10 = __load_font(f"{font_dir}FreeSans.ttf", 10)
-free_sans_12 = __load_font(f"{font_dir}FreeSans.ttf", 12)
-free_sans_16 = __load_font(f"{font_dir}FreeSans.ttf", 16)
+tahoma = __load_font(f"{__font_folder}tahoma.ttf", 30)
+dejavu = __load_font(f"{__font_folder}dejavu.ttf", 28)
+arial_10 = __load_font(f"{__font_folder}arial.ttf", 10)
+arial_12 = __load_font(f"{__font_folder}arial.ttf", 12)
+free_sans_10 = __load_font(f"{__font_folder}FreeSans.ttf", 10)
+free_sans_12 = __load_font(f"{__font_folder}FreeSans.ttf", 12)
+free_sans_16 = __load_font(f"{__font_folder}FreeSans.ttf", 16)
 
 
-def test():
-    oled_font = ImageFont.truetype('fonts/FreeSans.ttf', 12)
-    with canvas(oled) as draw:
-        draw.rectangle(oled.bounding_box, outline="black", fill="white")
-        draw.text((10, 10), "OLED-Display", font=oled_font, fill="black")
+def __show_humidity(x=left, font=ImageFont.load_default(), wait=5.0):
+    hum = get_values()[0]
+    with canvas(display) as draw:
+        draw.text((x, top),
+                  "Humidity", font=ImageFont.load_default(), fill=255)
+        draw.text((x, display.height / 3 - 3),
+                  hum, font=font, fill=255)
+    sleep(wait)
 
 
-def __show_hum_temp(line1, line2, x=left, font=ImageFont.load_default()):
+def __show_temperature(x=left, font=ImageFont.load_default(), wait=5.0):
+    temp = get_values()[1]
+    with canvas(display) as draw:
+        draw.text((x, top),
+                  "Temperature", font=ImageFont.load_default(), fill=255)
+        draw.text((x, display.height / 3 - 3),
+                  temp, font=font, fill=255)
+    sleep(wait)
+
+
+def __show_hum_temp(line1, line2, x=left,
+                    font=ImageFont.load_default(), wait=5.0):
     hum, temp = get_values()[0], get_values()[1]
-    oled.clear()
-    with canvas(oled) as draw:
-        # Write the lines of text.
+    with canvas(display) as draw:
         draw.text((x, line1), f"H {hum}", font=font, fill=255)
-        draw.text((x, line2), f"T  {temp}", font=font, fill=255)
+        draw.text((x, line2), f"T {temp}", font=font, fill=255)
+    sleep(wait * 2)
+
+
+def __show_separate_hum_temp_(x=left, font=ImageFont.load_default(), wait=5.0):
+    hum, temp = get_values()[0], get_values()[1]
+    with canvas(display) as draw:
+        draw.text((x, top),
+                  "Humidity", font=ImageFont.load_default(), fill=255)
+        draw.text((x, display.height / 3 - 3),
+                  hum, font=font, fill=255)
+    sleep(wait)
+    with canvas(display) as draw:
+        draw.text((x, top),
+                  "Temperature", font=ImageFont.load_default(), fill=255)
+        draw.text((x, display.height / 3 - 3),
+                  temp, font=font, fill=255)
+    sleep(wait)
 
 
 def __get_core_temp():
@@ -76,20 +104,19 @@ def __get_core_temp():
     return temp_str
 
 
-def __show_pi():
-    oled.clear()
-    # image inverted
-    with canvas(oled) as draw:
+def __show_pi(wait=5.0):
+    with canvas(display) as draw:
         draw.rectangle((32, top - 3, 95, 63), outline=1, fill=1)
         draw.bitmap((32, top - 3), Image.open('pi_logo.png'), fill=0)
+    sleep(wait)
 
 
-def __show_state(font=ImageFont.load_default()):
+def __show_state(font=ImageFont.load_default(), single_line=False, wait=5.0):
     """
     Shell scripts for system monitoring from here :
     https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
     """
-    oled.clear()
+
     # IP
     cmd = "hostname -I | cut -d\' \' -f1"
     ip = subprocess.check_output(cmd, shell=True).decode('utf-8')
@@ -110,26 +137,50 @@ def __show_state(font=ImageFont.load_default()):
     cmd = "df -h | awk '$NF==\"/\"{printf \"Disk : %d / %d GB %s\", $3,$2,$5}'"
     disk = subprocess.check_output(cmd, shell=True).decode('utf-8')
 
-    with canvas(oled) as draw:
-        # Write the lines of text.
-        draw.text((left, top), f"IP: {ip}", font=font, fill=255)
-        draw.text((left, top + 15), cpu, font=font, fill=255)
-        draw.text((left, top + 30), mem_usage, font=font, fill=255)
-        draw.text((left, top + 45), disk, font=font, fill=255)
+    if single_line:
+        with canvas(display) as draw:
+            draw.text((left, display.height / 3),
+                      f"IP: {ip}", font=font, fill=255)
+            sleep(wait)
+        with canvas(display) as draw:
+            draw.text((left, display.height / 3),
+                      cpu, font=font, fill=255)
+            sleep(wait)
+        with canvas(display) as draw:
+            draw.text((left, display.height / 3),
+                      mem_usage, font=font, fill=255)
+            sleep(wait)
+        with canvas(display) as draw:
+            draw.text((left, display.height / 3),
+                      mem_usage, font=font, fill=255)
+            sleep(wait)
+    else:
+        with canvas(display) as draw:
+            draw.text((left, top),
+                      f"IP: {ip}", font=font, fill="white")
+            draw.text((left, top + 15),
+                      cpu, font=font, fill="white")
+            draw.text((left, top + 30),
+                      mem_usage, font=font, fill="white")
+            draw.text((left, top + 45),
+                      disk, font=font, fill="white")
+        sleep(wait * 2)
 
 
 def run_at_128x64():
-    __show_state(font=free_sans_10)
-    sleep(5)
-    __show_hum_temp(top + 3, top + 35, x=5, font=dejavu)
-    sleep(15)
+    __show_state(font=free_sans_10, wait=2)
+    __show_humidity(x=5, font=tahoma)
+    # __show_state(font=free_sans_10)
+    __show_temperature(x=5, font=tahoma)
+    # __show_state(font=free_sans_10)
+    # __show_separate_hum_temp_(x=5, font=dejavu)
+    __show_state(font=free_sans_10, wait=2)
+    __show_hum_temp(top + 3, top + 35, x=5, font=tahoma)
 
 
 def run_at_128x32():
-    # __show_state()
-    # sleep(5)
+    __show_state(font=free_sans_10, single_line=True)
     __show_hum_temp(top + 5, top + 37, font=tahoma)
-    sleep(15)
 
 
 if __name__ == '__main__':
@@ -138,12 +189,12 @@ if __name__ == '__main__':
 
         try:
             run_at_128x64()
-            # run_at_128x32()
 
         except KeyboardInterrupt:
             stderr.write("Oled interrupted.\n")
-            oled.clear()
+            display.clear()
             exit()
 
         except Exception as e:
             stderr.write(f"OLED: Any error or exception occurred! , {e}\n")
+            display.clear()
