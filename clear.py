0a33f0f11e42cc0dcad07b66a1f0257ece6357e6
Thomas <kaulketh@gmail.com>
2021-05-05 23:11:46 +0200

    Add clear.py

---
+++ clear.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# -----------------------------------------------------------
+# created 02.02.2021
+# Thomas Kaulke, kaulketh@gmail.com
+# https://github.com/kaulketh
+# -----------------------------------------------------------
+
+
+if __name__ == '__main__':
+    from oled import display
+
+    display.clear()
